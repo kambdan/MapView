2adcad52bd267c38f0bada5b796971fcf1149464
kambdan <juancambi1319@gmail.com>
2023-07-25 10:12:35 -0500

    Initial Commit

---
+++ helloWorld/helloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldApp.swift
+//  helloWorld
+//
+//  Created by Juan Cambizaca on 25/7/23.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
